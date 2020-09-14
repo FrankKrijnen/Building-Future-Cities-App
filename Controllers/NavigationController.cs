846f92c2f2a945258342bacb64256781921cf7bc
Georgi Atanasov <gaterin98@gmail.com>
2020-09-15 00:33:03 +0200

    Created wireframe design with HTML/CSS

---
+++ Controllers/NavigationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildingFutureCitiesApp.Controllers
+{
+    public class NavigationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Livingroom()
+        {
+            ViewBag.Message = "Livingroom";
+            ViewBag.Title = "Set up the " + ViewBag.Message;
+            return View();
+        }
+
+        public ActionResult Kitchen()
+        {
+            ViewBag.Message = "Kitchen";
+            ViewBag.Title = "Set up the " + ViewBag.Message;
+            return View();
+        }
+        public ActionResult Bathroom()
+        {
+            ViewBag.Message = "Bathroom";
+            ViewBag.Title = "Set up the " + ViewBag.Message;
+            return View();
+        }
+
+        public ActionResult Bedroom()
+        {
+            ViewBag.Message = "Bedroom";
+            ViewBag.Title = "Set up the " + ViewBag.Message;
+            return View();
+        }
+    }
+}

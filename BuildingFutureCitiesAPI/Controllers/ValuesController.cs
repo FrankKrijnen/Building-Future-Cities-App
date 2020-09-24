e4c10794a9dd2125730ffed2da9cdc16eff7d24c
Georgi Atanasov <gaterin98@gmail.com>
2020-09-24 23:54:26 +0200

    Add CRUD request recievers in API Add Material model

--- BuildingFutureCitiesAPI/Controllers/ValuesController.cs
+++ BuildingFutureCitiesAPI/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BuildingFutureCitiesAPI.Models;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 
 namespace BuildingFutureCitiesAPI.Controllers
 {
@@ -21,25 +23,48 @@ namespace BuildingFutureCitiesAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            Material material = new Material(id, "Metal Door Handle");
+            return Ok($"Material found with id:{id} name: {material.Name}");
+        }
+
+        // GET api/values/create/4/radiator
+        [HttpGet("create/{id}/{name}")]
+        public ActionResult<string> Get(int id, string name)
+        {
+          
+            Material material = new Material(id,name);
+            return Ok($"Material created in database with id: {material.Id} and name: {material.Name}");
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<string> Post([FromBody] string value,string value2)
         {
+            if (value == "hey")
+            {
+                return Ok(value + value2);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // PUT api/values/update/4/radiator
+        [HttpPut("update/{id}/{newName}")]
+        public ActionResult<string> Put(int id, string newName)
         {
+            Material material = new Material(id, "Wooden Door");
+            return Ok($"Material Updated in database With id: {id} and name: {material.Name} to name : {newName}");
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<string> Delete(int id)
         {
+            Material material = new Material(id,"Wooden Door");
+            return Ok($"Material Deleted in database With id: {id} and name: {material.Name}");
         }
     }
 }
---
+++ BuildingFutureCitiesAPI/Models/Material.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingFutureCitiesAPI.Models
+{
+    public class Material
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Material()
+        {
+            Id = 0;
+            Name = "";
+        }
+        public Material(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        public int GetMaterialId()
+        {
+            return Id;
+        }
+        public string GetMaterialName()
+        {
+            return Name;
+        }
+        public void SetMaterialId(int id)
+        {
+            this.Id = id;
+        }
+        public void SetMaterialName(string name)
+        {
+            this.Name = name;
+        }
+
+
+    }
+}

e05885ed0e75f8ad347ddb70a94bf2f5ba039ac4
Joel Ipenburg <joel.ipenburg@student.hu.nl>
2020-09-28 17:41:59 +0200

    Added configuration class for saving object configurations

---
+++ Models/ConfigurationClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildingFutureCitiesApp.Models
+{
+    public class ConfigurationClass
+    {
+        
+        //constructor
+        public ConfigurationClass(string roomName, double currentConceptSCI, int currentConceptCO2, double newConceptSCI, int newConceptCO2)
+        {
+            RoomName = roomName;
+            CurrentConceptSCI = currentConceptSCI;
+            CurrentConceptCO2 = currentConceptCO2;
+            NewConceptSCI = newConceptSCI;
+            NewConceptCO2 = newConceptCO2;
+        }
+
+        //properties
+        public string RoomName;
+
+        //current concept
+        public double CurrentConceptSCI;
+        public int CurrentConceptCO2;
+
+        //new concept
+        public double NewConceptSCI;
+        public int NewConceptCO2;
+
+    }
+}

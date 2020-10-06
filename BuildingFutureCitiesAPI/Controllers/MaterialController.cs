be263b772a68c111b3746f9a4434a78f4e44bd2f
Gijs Veldhuysen <gijs.veldhuysen@student.hu.nl>
2020-10-06 23:13:18 +0200

    248 setup frontend, 249 gather variables, 251 setup request, 252 set profile into database

--- BuildingFutureCitiesAPI/Controllers/MaterialController.cs
+++ BuildingFutureCitiesAPI/Controllers/MaterialController.cs
@@ -7,6 +7,9 @@ using BuildingFutureCitiesAPI.Models;
 using BuildingFutureCitiesAPI.DataModels;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using System.Web.UI.WebControls;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+using System.Web.WebPages;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -49,14 +52,25 @@ namespace BuildingFutureCitiesAPI.Controllers
 
         // POST api/<MaterialController>
         [HttpPost]
+              
         public void SetMaterial([FromForm] string ProductName, [FromForm] int LiveArea, [FromForm] int ObjectLiveAreaFunction,
             [FromForm] int Removability, [FromForm] int MaterialOrigins,
             [FromForm] int MaterialDistance, [FromForm] int Unit_Kg_M2_Amount, [FromForm] decimal EmbodiedEnergie,
-            [FromForm] decimal EmbodiedCO2, [FromForm] int LifeSpan)
-        {
-            string qry = "INSERT INTO materials (materials_id, material, estate_object_id, estate_area_id, function_id, units, origin_id, distance_id, embodied_energy, embodied_co2, lifespan, removability_id, image) VALUES (0, '" + @ProductName + "', '" + 1 + "', '" + @LiveArea + "', '" + @ObjectLiveAreaFunction + "', '" + @Unit_Kg_M2_Amount + "', '" + @MaterialOrigins + "', '" + @MaterialDistance + "', '" + @EmbodiedEnergie + "', '" + @EmbodiedCO2 + "', '" + @LifeSpan + "', '" + @Removability + "', 'hgfhfgjhfghj');";
-            Constructor();
-            materialDataModel.SetMaterialItem(qry);
+            [FromForm] decimal EmbodiedCO2, [FromForm] int LifeSpan, [FromForm] string FirstName, 
+            [FromForm] string LastName, [FromForm] string Email, [FromForm] string Password, [FromForm] string PasswordRepeat)
+        {          
+
+                string qry = "INSERT INTO materials (materials_id, material, estate_object_id, estate_area_id, function_id, units, origin_id, distance_id, embodied_energy, embodied_co2, lifespan, removability_id, image) VALUES (0, '" + @ProductName + "', '" + 1 + "', '" + @LiveArea + "', '" + @ObjectLiveAreaFunction + "', '" + @Unit_Kg_M2_Amount + "', '" + @MaterialOrigins + "', '" + @MaterialDistance + "', '" + @EmbodiedEnergie + "', '" + @EmbodiedCO2 + "', '" + @LifeSpan + "', '" + @Removability + "', 'hgfhfgjhfghj');";
+                Constructor();
+                materialDataModel.SetMaterialItem(qry);
+
+                string qry2 = "INSERT INTO profile (id, firstname, lastname, email, password) VALUES (0, '" + @FirstName + "', '" + @LastName + "', '" + @Email + "', '" + @Password + "');";
+                Constructor();
+                materialDataModel.SetRegisterProfile(qry2);
+
+                Response.Redirect("https://localhost:44355/");
+
+                 
         }
 
         // PUT api/<MaterialController>/5

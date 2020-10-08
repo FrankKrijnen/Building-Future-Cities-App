4b39eec13f3e7b673034d5796eccd0bbdf64b3aa
Joel Ipenburg <joel.ipenburg@student.hu.nl>
2020-10-08 20:50:48 +0200

    Added loginvalidation model to validate logincredentials

---
+++ Models/LoginValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuildingFutureCitiesApp.Models
+{
+
+    /// <summary>
+    ///  <see cref="LoginValidation"/> class which uses <see cref="Profile"/> class to login
+    /// </summary>
+    public class LoginValidation
+    {
+        [Required(ErrorMessage = "Emailadres is vereist")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    
+
+        [Required(ErrorMessage = "Wachtwoord is vereist")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool isLoggedIn;
+        public Profile loggedInProfile;
+
+    }
+}

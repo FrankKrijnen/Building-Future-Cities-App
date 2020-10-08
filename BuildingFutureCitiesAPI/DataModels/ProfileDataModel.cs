4c11650df88568b3f3d057824d62dbe1c4adea4e
Joel Ipenburg <joel.ipenburg@student.hu.nl>
2020-10-08 20:47:44 +0200

    Added profiledatamodel to retrieve profiledata

---
+++ BuildingFutureCitiesAPI/DataModels/ProfileDataModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildingFutureCitiesAPI.Models;
+using MySql.Data.MySqlClient;
+
+namespace BuildingFutureCitiesAPI.DataModels
+{
+    public class ProfileDataModel : DataModel
+    {
+        
+        public Profile GetProfileData(string qry)
+        {
+            Profile profile = new Profile();
+
+            using (MySqlCommand preparedQry = GetDatabaseConnection().PrepareSql(qry))
+            {
+
+                try
+                {
+                    if (GetDatabaseConnection().Connection.State == ConnectionState.Closed)
+                    {
+                        GetDatabaseConnection().Connection.Open();
+                    }
+
+                    using (MySqlDataReader reader = preparedQry.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            profile.FirstName = reader["firstname"].ToString();
+                            profile.LastName = reader["lastname"].ToString();
+                            profile.Email = reader["email"].ToString();
+                        }
+                    }
+                    GetDatabaseConnection().Connection.Close();
+                }
+                catch (Exception e)
+                {
+                    SetErrorMsg(e.Message.ToString());
+                }
+
+            }
+
+            return profile;
+        }
+    }
+
+}

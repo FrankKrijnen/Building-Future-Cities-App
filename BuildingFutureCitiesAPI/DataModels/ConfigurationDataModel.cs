ec5ec1db2ec31aaea2e274d9c95dae24c47878c7
Joel Ipenburg <joel.ipenburg@student.hu.nl>
2020-10-25 22:44:36 +0100

    Added configuration datamodel getter and setter

---
+++ BuildingFutureCitiesAPI/DataModels/ConfigurationDataModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildingFutureCitiesAPI.Models;
+using MySql.Data.MySqlClient;
+
+namespace BuildingFutureCitiesAPI.DataModels
+{
+    public class ConfigurationDataModel : DataModel
+    {
+        public void SetConfiguration(string qry)
+        {
+            try
+            {
+                if (GetDatabaseConnection().Connection.State == ConnectionState.Closed)
+                {
+                    GetDatabaseConnection().Connection.Open();
+                }
+                using (MySqlCommand command = GetDatabaseConnection().PrepareSql(qry))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            GetDatabaseConnection().Connection.Close();
+        }
+
+        public int GetConfiguration(string qry)
+        {
+            int configurationId = new Int32();
+
+            using (MySqlCommand preparedQry = GetDatabaseConnection().PrepareSql(qry))
+            {
+
+                try
+                {
+                    if (GetDatabaseConnection().Connection.State == ConnectionState.Closed)
+                    {
+                        GetDatabaseConnection().Connection.Open();
+                    }
+
+                    using (MySqlDataReader reader = preparedQry.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            configurationId = Convert.ToInt32(reader["id"]);
+                        }
+                    }
+                    GetDatabaseConnection().Connection.Close();
+                }
+                catch (Exception e)
+                {
+                    SetErrorMsg(e.Message.ToString());
+                }
+
+            }
+
+            return configurationId;
+        }
+    }
+}
+

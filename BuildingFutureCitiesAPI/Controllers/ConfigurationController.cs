a51322c30c2afa031eea99eeacea1b3473781909
Joel Ipenburg <joel.ipenburg@student.hu.nl>
2020-10-25 22:43:06 +0100

    Added setconfiguration method to insert configuration into db

--- BuildingFutureCitiesAPI/Controllers/ConfigurationController.cs
+++ BuildingFutureCitiesAPI/Controllers/ConfigurationController.cs
@@ -17,12 +17,47 @@ namespace BuildingFutureCitiesAPI.Controllers
     [ApiController]
     public class ConfigurationController : ControllerBase
     {
+        private ConfigurationDataModel configurationDataModel;
+        public void Constructor()
+        {
+            configurationDataModel = new ConfigurationDataModel();
+        }
+
         // POST api/<ConfigurationController>
         [HttpPost("SetConfiguration")]
-        public void SetConfiguration()
+        public void SetConfiguration([FromForm] int[] material_id, [FromForm] string description)
         {
-            Console.WriteLine("set config aangeroepen.");
-            Response.Redirect("https://localhost:44355/");
+            this.Constructor();
+           
+            //1 keer uitvoeren voor setup
+            string qry =
+                "INSERT INTO `configuration` (`id`, `description`, `room`, `profile_id`) VALUES (NULL, '"+ @description +"', 'badkamer', '1');";
+            configurationDataModel.SetConfiguration(qry);
+            
+            //krijg id terug van net gemaakte configuratie
+            string qry2 = "SELECT id FROM `configuration` WHERE description = '" + @description + "' AND room = 'badkamer'";
+            int configurationId = configurationDataModel.GetConfiguration(qry2);
+
+            //meerdere keren uitvoeren 
+            string partOfQry3 = string.Empty;
+            int materialIdIndex = material_id.Length;
+            foreach (int materialId in material_id)
+            {
+                //checkt op laatste material
+                if (materialIdIndex.Equals(1))
+                {
+                    partOfQry3 += "(NULL, '" + @configurationId + "', '" + materialId + "')";
+                    break;
+                }
+                partOfQry3 += "(NULL, '"+ @configurationId + "', '"+ materialId + "'),";
+                materialIdIndex--;
+            }
+            string qry3 =
+                "INSERT INTO `configuration_material` (`id`, `configuration_id`, `material_id`) VALUES" + partOfQry3 + ";";
+            configurationDataModel.SetConfiguration(qry3);
+
+            Response.Redirect("https://localhost:44355/Navigation/Configuration");
         }
+        
     }
 }

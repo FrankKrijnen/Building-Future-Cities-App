de4776e2752d4c9606aaa88cfd462192417a7878
Georgi Atanasov <gaterin98@gmail.com>
2020-11-20 17:38:49 +0100

    added front end buttons for CSV import

---
+++ Controllers/ImportMaterialController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildingFutureCitiesApp.Controllers
+{
+    public class ImportMaterialController : Controller
+    {
+        // GET: ImportMaterial
+        public ActionResult CSV()
+        {
+            ViewBag.ImportMessage = "Importeer bevestiging";
+            return View();
+        }
+    }
+}
